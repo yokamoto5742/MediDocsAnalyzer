457ae6b6c1b5b0a21838dfbb0817562c14702969
yokamoto5742-h <yokamoto5742@gmail.com>
2025-03-08 15:09:39 +0900

    service_medical_docs_analyzer.pyの更新

--- service_medical_docs_analyzer.py
+++ service_medical_docs_analyzer.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+
 from config_manager import load_config
 from service_analyze_medical_documents import analyze_medical_documents
 
@@ -10,12 +11,9 @@ class MedicalDocsAnalyzer:
 
     def run_analysis(self, start_date_str, end_date_str):
         try:
-            # 設定ファイルからパスを取得
             database_path = self.paths_config['database_path']
             template_path = self.paths_config['template_path']
 
-            # 医療文書の分析を実行
-            # 開始日と終了日のパラメータを追加
             analyze_medical_documents(database_path, template_path, start_date_str, end_date_str)
 
             return True, "集計が完了しました。"
